409082a589831987e9288e23fb1a73d4882cb01f
Mario Fernández <nilovinilo1@gmail.com>
2023-06-08 21:44:26 +0200

    NavigationBar with Logo box-sizing: border-box for all the component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,21 @@
 import { Fragment } from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
-        <h1>I am the Navigation Bar</h1>
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
       </div>
       <Outlet />
     </Fragment>
