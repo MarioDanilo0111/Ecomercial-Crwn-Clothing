37ba1b2c3d0605d9e8d2539423de0b6e8f9df18f
Mario Fernández <nilovinilo1@gmail.com>
2023-06-09 10:43:31 +0200

    Firebase settup Sett up Firebase Authentication Flow Auth with Firebase / process REACT_APP

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APP_KEY,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
