8238f56c98412e5da24d3859f8edf3cb096b4ee5
Mario Fernández <nilovinilo1@gmail.com>
2023-06-11 12:30:34 +0200

    Sign up form saving user to db reset func working with additional data too

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,3 +1,4 @@
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
@@ -14,6 +15,7 @@ const SignIn = () => {
     <div className="sign-in">
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
