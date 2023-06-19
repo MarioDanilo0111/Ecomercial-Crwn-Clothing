d01e5c50db5afee26eb75696940d6259e8b33d58
Mario Fernández <nilovinilo1@gmail.com>
2023-06-19 13:46:07 +0200

    Quantety funtionality to Cart Icon now the number increases

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const addCartItem = (cartItem, productToAdd) => {
   // find if cartItem contains productToAdd
@@ -25,17 +25,36 @@ export const CartContext = createContext({
   /* add cart to dropdow */
   cartItems: [],
   addItemToCart: () => {},
+  /* cart counter */
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   /* add cart to dropdow */
   const [cartItems, setCartImems] = useState([]);
+  /* count setter, increasing the count */
+  const [cartCount, setCartCount] = useState(0);
+
+  /* useEffect to add the total of items added */
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
     setCartImems(addCartItem(cartItems, productToAdd));
   };
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    cartCount,
+  };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
